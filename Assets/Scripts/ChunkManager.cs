5eb904ac62f0ee75e2dd87eb981a35675cec3c9c
Natesh Narain <nnaraindev@gmail.com>
2018-01-04 00:01:52 -0500

    remove numChunks field

--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -8,7 +8,6 @@ public class ChunkManager : MonoBehaviour
     public GameObject player;
 
     public Chunk chunkPrefab;
-    public int numChunks;
 
     public float forwardRenderDistance;
     public float generalRenderDistance;
@@ -50,8 +49,7 @@ public class ChunkManager : MonoBehaviour
         {
             lastPlayerPosition = playerPosition;
 
-            UpdateSurroundingChunks(playerPosition);
-            //UpdateVisibleChunks(playerPosition);
+            UpdateVisibleChunks(playerPosition);
             RemoveFarChunks(player.transform.position);
         }
     }
@@ -65,6 +63,7 @@ public class ChunkManager : MonoBehaviour
         if (rotateDiff >= rotationThreshold)
         {
             lastPlayerRotation = playerRotation;
+            UpdateVisibleChunks(player.transform.position);
         }
     }
 
@@ -72,7 +71,7 @@ public class ChunkManager : MonoBehaviour
     /// Use a breath-first search to queue surrounding chunks for loading
     /// </summary>
     /// <param name="playerPosition"></param>
-    private void UpdateSurroundingChunks(Vector3 playerPosition)
+    private void UpdateVisibleChunks(Vector3 playerPosition)
     {
         // the player's chunk position
         Vector3Int playerChunkPosition = GetChunkPosition(playerPosition);
@@ -148,56 +147,6 @@ public class ChunkManager : MonoBehaviour
                 chunkLoader.Enqueue(chunk);
             }
         }
-    }
-
-    private void UpdateVisibleChunks(Vector3 playerPosition)
-    {
-        // get the players chunk position
-        Vector3Int playerChunkPosition = GetChunkPosition(playerPosition);
-
-        // loop for the chunks surrounding the player
-        for (int x = playerChunkPosition.x - numChunks; x < playerChunkPosition.x + numChunks; ++x)
-        {
-            for (int z = playerChunkPosition.z - numChunks; z < playerChunkPosition.z + numChunks; ++z)
-            {
-                Vector3Int chunkPosition = new Vector3Int(x, 0, z);
-
-                // check if the chunk already exists in the dictionary
-                if (chunkList.ContainsKey(chunkPosition))
-                {
-                    // if it does, ensure that it is enabled
-                    var chunk = chunkList[chunkPosition];
-                    chunk.gameObject.SetActive(true);
-                }
-                else
-                {
-                    // if the chunk does not exist yet, create it.
-                    var chunk = CreateChunk(x, z);
-
-                    chunkList.Add(chunkPosition, chunk);
-
-                    chunkLoader.Enqueue(chunk);
-                }
-            }
-        }
-    }
-
-    private void FillViewFrustum()
-    {
-        CameraFrustum frustum = playerCamera.GetFrustum();
-
-        Vector3 corner1 = frustum.GetNearCorner(CameraFrustum.Corner.LB);
-        Vector3 corner2 = frustum.GetNearCorner(CameraFrustum.Corner.RB);
-
-        Vector3 corner1XZ = new Vector3(corner1.x, 0, corner1.z);
-        Vector3 corner2XZ = new Vector3(corner2.x, 0, corner2.z);
-
-        Vector3Int chunkPosition1 = GetChunkPosition(corner1XZ);
-        Vector3Int chunkPosition2 = GetChunkPosition(corner2XZ);
-
-
-
-        // search chunk neighbors and load the ones that lie within the camera frustum and are within a certain distance
     }
 
     private void RemoveFarChunks(Vector3 playerPosition)
@@ -307,7 +256,6 @@ public class ChunkManager : MonoBehaviour
 
     private void OnValidate()
     {
-        if (numChunks <= 0) numChunks = 1;
         if (moveThreshold <= 0) moveThreshold = 1f;
         if (distanceToInactive <= 0) distanceToInactive = 100f;
         if (distanceToDestroy <= 0) distanceToDestroy = 1f;
