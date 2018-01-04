0f48675d9648bfcda026b3a823bdd226894ae055
Natesh Narain <nnaraindev@gmail.com>
2018-01-03 21:56:55 -0500

    search chunks around the player to load using a breath-first search

--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -48,7 +48,8 @@ public class ChunkManager : MonoBehaviour
         {
             lastPlayerPosition = playerPosition;
 
-            UpdateVisibleChunks(playerPosition);
+            UpdateSurroundingChunks(playerPosition);
+            //UpdateVisibleChunks(playerPosition);
             RemoveFarChunks(player.transform.position);
         }
     }
@@ -62,6 +63,74 @@ public class ChunkManager : MonoBehaviour
         if (rotateDiff >= rotationThreshold)
         {
             lastPlayerRotation = playerRotation;
+        }
+    }
+
+    /// <summary>
+    /// Use a breath-first search to queue surrounding chunks for loading
+    /// </summary>
+    /// <param name="playerPosition"></param>
+    private void UpdateSurroundingChunks(Vector3 playerPosition)
+    {
+        // the player's chunk position
+        Vector3Int playerChunkPosition = GetChunkPosition(playerPosition);
+
+        // explored chunks
+        HashSet<Vector3Int> explored = new HashSet<Vector3Int>();
+
+        // queue of chunk positions
+        Queue<Vector3Int> queue = new Queue<Vector3Int>();
+
+        // load initial chunk positions to check
+        queue.Enqueue(playerChunkPosition);
+        foreach (var key in GetChunkNeighbors(playerChunkPosition))
+        {
+            queue.Enqueue(key);
+        }
+
+        // loop while items are in the queue
+        while (queue.Count > 0)
+        {
+            // grab first position
+            Vector3Int p = queue.Dequeue();
+
+            // explore neighbors
+            Vector3Int[] neighbors = GetChunkNeighbors(p);
+
+            foreach (var neighbor in neighbors)
+            {
+                // check that is doesn't already exist
+                if (!chunkList.ContainsKey(neighbor))
+                {
+                    // get the world position of the chunk
+                    Vector3 chunkPosition = GetWorldPositionFromChunkPosition(neighbor);
+
+                    // check the chunk is in the render distance of the player
+                    var distanceFromPlayer = (playerPosition - chunkPosition).magnitude;
+
+                    if (distanceFromPlayer <= renderDistance)
+                    {
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            // check if the current chunk is in the chunk list
+            if (chunkList.ContainsKey(p))
+            {
+                // the chunk already exists, ensure it is enabled
+                var chunk = chunkList[p];
+                chunk.gameObject.SetActive(true);
+            }
+            else
+            {
+                // the chunk does not exist yet, create it
+                var chunk = CreateChunk(p.x, p.z);
+                chunkList.Add(p, chunk);
+
+                // queue the chunk for loading
+                chunkLoader.Enqueue(chunk);
+            }
         }
     }
 
@@ -164,6 +233,17 @@ public class ChunkManager : MonoBehaviour
         return chunk;
     }
 
+    private Vector3Int[] GetChunkNeighbors(Vector3Int p)
+    {
+        return new Vector3Int[]
+        {
+            new Vector3Int(p.x + 1, 0, p.z),
+            new Vector3Int(p.x - 1, 0, p.z),
+            new Vector3Int(p.x, 0, p.z + 1),
+            new Vector3Int(p.x, 0, p.z - 1)
+        };
+    }
+
     private Vector3Int GetChunkPosition(Vector3 position)
     {
         Vector3Int chunkPosition = new Vector3Int();
@@ -173,6 +253,17 @@ public class ChunkManager : MonoBehaviour
         chunkPosition.z = (int)position.z / chunkPrefab.chunkSizeZ;
 
         return chunkPosition;
+    }
+
+    private Vector3 GetWorldPositionFromChunkPosition(Vector3Int chunkPosition)
+    {
+        Vector3 worldPosition = new Vector3();
+
+        worldPosition.x = chunkPosition.x * chunkPrefab.chunkSizeX;
+        worldPosition.y = chunkPosition.y * chunkPrefab.chunkSizeY;
+        worldPosition.z = chunkPosition.z * chunkPrefab.chunkSizeZ;
+
+        return worldPosition;
     }
 
     private void OnDrawGizmosSelected()
