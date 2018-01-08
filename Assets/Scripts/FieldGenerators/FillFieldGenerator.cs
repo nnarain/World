d8ce8aec21aef3bd8ffef0e6303495f751241917
Natesh Narain <nnaraindev@gmail.com>
2018-01-07 22:29:17 -0500

    Greedy meshing accounting for face orientation

---
+++ Assets/Scripts/FieldGenerators/FillFieldGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillFieldGenerator : FieldGenerator
+{
+    void FieldGenerator.Generate(Field field, Vector3 position)
+    {
+        field.ForEach((x, y, z, v) =>
+        {
+            field.Set(x, y, z, 1);
+        });
+    }
+}
