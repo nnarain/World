863e5f96dc29939711ed80b1925dbb1dbbef7110
Natesh Narain <nnaraindev@gmail.com>
2018-01-12 22:42:37 -0500

    update

--- Assets/Scripts/FieldGenerators/ElevationMoistureFieldGenerator.cs
+++ Assets/Scripts/FieldGenerators/ElevationMoistureFieldGenerator.cs
@@ -8,11 +8,13 @@ public class ElevationMoistureFieldGenerator : FieldGenerator
     {
         Air,
         Water,
-        Sand,
-        Dirt,
-        Grass,
-        Stone,
-        Snow
+        TemperateForest,
+        DeciduousForset,
+        Grassland,
+        TemperateDesert,
+        RainForset,
+        Snow,
+        Tundra
     }
 
     [System.Serializable]
@@ -36,6 +38,8 @@ public class ElevationMoistureFieldGenerator : FieldGenerator
     public AnimationCurve heightToTemperature;
 
     public Color[] voxelColors = new Color[Enum.GetNames(typeof(VoxelType)).Length];
+
+    private const float SEA_LEVEL = 4f;
 
     public override void Generate(VoxelField field, Vector3 position)
     {
@@ -67,14 +71,73 @@ public class ElevationMoistureFieldGenerator : FieldGenerator
             float height = heightEval.Evaluate(heightMap[x, z]) * (float)(field.Y - 1);
             int y = Mathf.RoundToInt(height);
 
-            float temperature = heightToTemperatureEval.Evaluate(heightMap[x, z]).Remap(0, 1, -10, 30);
-            float moisture = moistureMap[x, z] * 400;
+            float temperature = heightToTemperatureEval.Evaluate(heightMap[x, z]).Remap(0, 1, 0, 4);
+            float moisture = moistureMap[x, z].Remap(0, 1, 0, 5);
 
             for (int i = y; i >= 0; --i)
             {
-                field.Set(x, i, z, (byte)WhittakerBiomes(temperature, moisture));
+                field.Set(x, i, z, (byte)Simple(height));
             }
         });
+    }
+
+    private VoxelType Simple(float elevation)
+    {
+        if (elevation <= SEA_LEVEL)
+        {
+            return VoxelType.Water;
+        }
+        else
+        {
+            return VoxelType.TemperateForest;
+        }
+    }
+
+    private VoxelType Whittaker(float temperature, float moisture)
+    {
+        if (temperature >= 3)
+        {
+            if (moisture >= 4)
+            {
+                return VoxelType.RainForset;
+            }
+            else if (moisture >= 2)
+            {
+                return VoxelType.Grassland;
+            }
+            else
+            {
+                return VoxelType.TemperateDesert;
+            }
+        }
+        else if (temperature >= 2)
+        {
+            if (moisture >=4)
+            {
+                return VoxelType.TemperateForest;
+            }
+            else if (moisture >= 2)
+            {
+                return VoxelType.DeciduousForset;
+            }
+            else
+            {
+                return VoxelType.TemperateDesert;
+            }
+        }
+        else
+        {
+            if (moisture >= 4)
+            {
+                return VoxelType.Snow;
+            }
+            else
+            {
+                return VoxelType.Tundra;
+            }
+        }
+        
+        return VoxelType.Air;
     }
 
     public override Color GetVoxelColor(byte type)
@@ -82,56 +145,4 @@ public class ElevationMoistureFieldGenerator : FieldGenerator
         return voxelColors[type];
     }
 
-    private VoxelType WhittakerBiomes(float temperature, float moisture)
-    {
-        if (moisture < 100)
-        {
-            if (temperature <= 30)
-            {
-                return VoxelType.Sand;
-            }
-            else if (temperature <= 15)
-            {
-                return VoxelType.Dirt;
-            }
-            else
-            {
-                return VoxelType.Snow;
-            }
-        }
-        else if (moisture < 200)
-        {
-            return VoxelType.Grass;
-        }
-        else
-        {
-            return VoxelType.Grass;
-        }
-    }
-
-    private VoxelType ElevationToVoxelType(int elevation, int max)
-    {
-        if (elevation < 3)
-        {
-            return VoxelType.Water;
-        }
-        else if (elevation < 5)
-        {
-            return VoxelType.Sand;
-        }
-        else if (elevation < 7)
-        {
-            return VoxelType.Dirt;
-        }
-        else if (elevation < 10)
-        {
-            return VoxelType.Grass;
-        }
-        else if (elevation < 60)
-        {
-            return VoxelType.Stone;
-        }
-
-        return VoxelType.Snow;
-    }
 }
